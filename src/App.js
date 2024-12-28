1a23355a05439785bdda5e3fcbc663d73559c2d5
washington nieto <washington_nieto@hotmail.com>
2024-12-27 22:08:48 -0500

    boceto inicial

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import freeCodeCampLogo from './imagenes/freecodecamp-logo.png';
 
 function App() {
   return (
-    <div className="App">
-      Tareas - React
+    <div className="Aplicacion-tareas">
+      <div className='freecodecamp-logo-contenedor' >
+        <img 
+          src={freeCodeCampLogo}
+          className='freecodecamp-logo' 
+        />
+      </div>
+      <div className='tareas-lista-principal'>
+        <h1>Mis Tareas</h1>
+        
+
+      </div>
     </div>
   );
 }
